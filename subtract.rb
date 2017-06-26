84d9f066d974e96a43457bd54045a969ec9fa49d
AFKoala <bubbles.murph@gmail.com>
2017-06-26 11:30:49 -0400

    June 23 Homework

---
+++ subtract.rb
@@ -0,0 +1,11 @@
+p "We're going to subtract some numbers!"
+
+puts "Give me a number: "
+num1 = gets.to_i
+
+puts "Give me another number:"
+num2 = gets.to_i
+
+answer = num1 - num2
+
+puts "#{num1} - #{num2} = #{answer}"
